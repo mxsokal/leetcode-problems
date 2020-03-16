5a0a2c07edce2e882c1e49c4c4a06578aef6324d
mxsokal <mxsokal@protonmail.com>
2020-03-16 15:59:40 -0400

    'Two Sum'[1] solution added

--- 1-two-sum/src/net/mxs/leetcode/problem/twosum/Solution.java
+++ 1-two-sum/src/net/mxs/leetcode/problem/twosum/Solution.java
@@ -6,7 +6,21 @@ import java.util.HashMap;
 import static java.util.Objects.requireNonNull;
 
 /*
-    Description: just use a hash table to store complements + original indexes
+    Problem Description:
+
+
+        Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+        Example: 
+
+        Given nums = [2, 7, 11, 15], target = 9,
+
+        Because nums[0] + nums[1] = 2 + 7 = 9,
+        return [0, 1].
+
+
+    Solution Description: just use a hash table to store complements + original indexes
 
     Pseudo Code:
 
